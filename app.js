5f2e917df1d3584e3480abc4e52527b4f9dd1664
Mitchell Klich <mitch@Mitchells-MacBook-Pro.local>
2017-10-24 09:32:23 -0400

    Starting a new more interactive personal website

---
+++ app.js
@@ -0,0 +1,22 @@
+$('#mainPic').addClass('animated fadeIn');
+$('#welcomeText').addClass('animated fadeInUp');
+$('#welcomeTextSecond').addClass('animated fadeInUp');
+
+
+$('document').ready(function() {
+  $(this).scrollTop(0);
+  $('a#entryButton').on('click', function(ev) {
+    $('#nonScroll').removeAttr('id');
+    if (this.hash !== "") {
+      ev.preventDefault();
+      var hash = this.hash;
+    $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function() {
+          window.location.hash = hash;
+        });
+      }
+  });
+});
+
+// Have the menu bar hidden when you first get to the page
